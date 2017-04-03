af62eba319a4ea9a15daa8ad2e134a6cedb31aec
Guy Morita <guymorita@gmail.com>
2017-04-03 16:54:21 +0700

    add hamburger menu

--- src/app/containers/Main.js
+++ src/app/containers/Main.js
@@ -1,30 +1,90 @@
 
 import React, { Component } from 'react'
 import {
+  Button,
   StyleSheet,
   Text,
   View
 } from 'react-native'
 import { connect } from 'react-redux'
-import _ from 'lodash'
-
+import NavigationBar from 'react-native-navbar'
+import Drawer from 'react-native-drawer'
 
 class Main extends Component {
+  leftButtonConfig = {
+    title: 'Profile',
+    handler: () => this.toggleHamburger(),
+  };
+
+  titleConfig = {
+    title: 'Chirper',
+  };
+
+  closeDrawer = () => {
+    this._drawer.close()
+  }
+
+  openDrawer = () => {
+    this._drawer.open()
+  }
+
+  toggleHamburger() {
+    console.log('toggling')
+    this._drawer._open ? this.closeDrawer() : this.openDrawer()
+  }
+
   render() {
     const { auth } = this.props
-    console.log('auth', auth)
-    // alert(auth && auth.credentials && auth.credentials.stringify())
+
     return (
-      <View>
-        <Text>
-          Main
-        </Text>
-      </View>
+      <Drawer
+        ref={(ref) => this._drawer = ref}
+        content={<Profile closeDrawer={this.closeDrawer}/>}
+        openDrawerOffset={100}
+        >
+        <View style={styles.container}>
+          <NavigationBar
+            title={this.titleConfig}
+            leftButton={this.leftButtonConfig}
+          />
+        </View>
+      </Drawer>
     );
   }
 }
+
 const styles = StyleSheet.create({
 })
+
+class Profile extends Component {
+  close = () => {
+    this.props.closeDrawer()
+  }
+  render() {
+    return (
+      <View style={drawerStyles.container}>
+        <View>
+          <Button
+            onPress={this.close.bind(this)}
+            title={'Close'}
+          />
+          <Text>
+            Profile
+          </Text>
+        </View>
+      </View>
+
+    );
+  }
+}
+
+const drawerStyles = StyleSheet.create({
+  container: {
+    backgroundColor: '#4E4E4E',
+    flex: 1
+  }
+})
+
 
 const mapStateToProps = function(state) {
   const { auth } = state
